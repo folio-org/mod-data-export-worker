a856db4a33f46ba2c19321320b5f22c2e3526886
Aleksei Prokhorov <Aleksei_Prokhorov@epam.com>
2021-03-12 11:20:56 +0300

    MODEXPS-1::Create mod-data-export-spring and mod-data-export-worker modules basing on the PoC More logging.

--- src/main/java/org/folio/dew/batch/circulationlog/CirculationLogItemWriterFactory.java
+++ src/main/java/org/folio/dew/batch/circulationlog/CirculationLogItemWriterFactory.java
@@ -1,5 +1,6 @@
 package org.folio.dew.batch.circulationlog;
 
+import lombok.extern.log4j.Log4j2;
 import org.folio.dew.domain.dto.LogRecord;
 import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
@@ -8,30 +9,19 @@ import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
 @Component
+@Log4j2
 public class CirculationLogItemWriterFactory {
 
   public FlatFileItemWriter<LogRecord> getFlatFileItemWriter(String outputFilePath) {
-    final String commaDelimiter = ",";
-
     Resource outputFile = new FileSystemResource(outputFilePath);
-
-    return new FlatFileItemWriterBuilder<LogRecord>()
-        .name("circulationLogWriter")
+    log.info("Creating file {}.", outputFilePath);
+    return new FlatFileItemWriterBuilder<LogRecord>().name("circulationLogWriter")
         .resource(outputFile)
         .delimited()
-        .delimiter(commaDelimiter)
-        .names(
-            "id",
-            "eventId",
-            "userBarcode",
-            "items",
-            "object",
-            "action",
-            "date",
-            "servicePointId",
-            "source",
-            "description",
+        .delimiter(",")
+        .names("id", "eventId", "userBarcode", "items", "object", "action", "date", "servicePointId", "source", "description",
             "linkToIds")
         .build();
   }
+
 }

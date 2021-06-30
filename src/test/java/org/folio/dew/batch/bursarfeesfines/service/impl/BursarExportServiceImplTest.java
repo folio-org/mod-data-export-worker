f34b53d0c28b6d996b947f6a13aa5c99d9807f95
Roman Leshchenko <76975537+Roman-Leshchenko@users.noreply.github.com>
2021-06-30 16:01:47 +0300

    MODEXPW-22 Bursar export failed for large patron group (#89)
    * MODEXPW-22 fetch data in batch

---
+++ src/test/java/org/folio/dew/batch/bursarfeesfines/service/impl/BursarExportServiceImplTest.java
@@ -0,0 +1,89 @@
+package org.folio.dew.batch.bursarfeesfines.service.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.folio.dew.client.AccountBulkClient;
+import org.folio.dew.client.AccountClient;
+import org.folio.dew.client.FeefineactionsClient;
+import org.folio.dew.client.ServicePointClient;
+import org.folio.dew.client.TransferClient;
+import org.folio.dew.client.UserClient;
+import org.folio.dew.domain.dto.Account;
+import org.folio.dew.domain.dto.AccountdataCollection;
+import org.folio.dew.domain.dto.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.MockBeans;
+
+@SpringBootTest(classes = BursarExportServiceImpl.class)
+@MockBeans({
+  @MockBean(UserClient.class),
+  @MockBean(AccountBulkClient.class),
+  @MockBean(FeefineactionsClient.class),
+  @MockBean(TransferClient.class),
+  @MockBean(ServicePointClient.class)
+})
+class BursarExportServiceImplTest {
+  @Autowired
+  private BursarExportServiceImpl bursarExportService;
+  @MockBean
+  private AccountClient client;
+
+  @Test
+  @DisplayName("Find accounts should return empty list for not defined outStanding days")
+  void findAccountsEmptyTest() {
+    final List<User> users = generateUsers(100);
+    final List<Account> accounts = bursarExportService.findAccounts(null, users);
+
+    assertTrue(accounts.isEmpty());
+  }
+
+  @Test
+  @DisplayName("Find accounts should fetch data in several buckets")
+  void findAccountsTest() {
+    when(client.getAccounts(any(), eq(10000L))).thenReturn(mockAccountData());
+
+    final List<User> users = generateUsers(100);
+    final List<Account> accounts = bursarExportService.findAccounts(1L, users);
+
+    assertEquals(2, accounts.size());
+  }
+
+  @Test
+  @DisplayName("Find accounts should fetch data in one call")
+  void findAccountsLessThanBucketSizeTest() {
+    when(client.getAccounts(any(), eq(10000L))).thenReturn(mockAccountData());
+
+    final List<User> users = generateUsers(50);
+    final List<Account> accounts = bursarExportService.findAccounts(1L, users);
+
+    assertEquals(1, accounts.size());
+  }
+
+  private List<User> generateUsers(int size) {
+    return Stream
+      .generate(UUID::randomUUID)
+      .map(UUID::toString)
+      .map(id -> new User().id(id))
+      .limit(size)
+      .collect(Collectors.toList());
+  }
+
+  private AccountdataCollection mockAccountData() {
+    AccountdataCollection accounts = new AccountdataCollection();
+    Account account = new Account().id(UUID.randomUUID().toString());
+    accounts.accounts(List.of(account));
+    return accounts;
+  }
+}

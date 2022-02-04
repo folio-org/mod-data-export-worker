9fd8452891db4d0b2576ef01dd42383a39a4e20d
Siarhei Charniak <siarhei_charniak@epam.com>
2022-02-04 18:49:32 +0300

    MODEXPW-28 - Tenant deletion failed due to hsql error

---
+++ src/test/java/org/folio/dew/controller/TenantControllerTest.java
@@ -0,0 +1,31 @@
+package org.folio.dew.controller;
+
+import lombok.SneakyThrows;
+import org.folio.dew.BaseBatchTest;
+import org.folio.spring.integration.XOkapiHeaders;
+import org.folio.tenant.domain.dto.TenantAttributes;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class TenantControllerTest extends BaseBatchTest {
+  @Test
+  @SneakyThrows
+  void canDeleteTenantTest() {
+    var headers = defaultHeaders();
+    headers.put(XOkapiHeaders.TENANT, List.of("test_tenant"));
+
+    mockMvc.perform(post("/_/tenant").content(asJsonString(new TenantAttributes().moduleTo("mod-data-export-worker")))
+      .headers(headers)
+      .contentType(APPLICATION_JSON)).andExpect(status().isNoContent());
+
+    mockMvc.perform(delete("/_/tenant/test_tenant")
+      .headers(headers)
+      .contentType(APPLICATION_JSON)).andExpect(status().isNoContent());
+  }
+}

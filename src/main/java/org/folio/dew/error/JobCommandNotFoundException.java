b4bd3ddabaab56b73520e67ff7a7e6d18d1db96a
Siarhei Charniak <siarhei_charniak@epam.com>
2022-04-20 07:56:13 +0300

    * minor improvements

--- src/main/java/org/folio/dew/error/JobCommandNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.folio.dew.error;
-
-public class JobCommandNotFoundException extends RuntimeException {
-  public JobCommandNotFoundException(String message) {
-    super(message);
-  }
-}

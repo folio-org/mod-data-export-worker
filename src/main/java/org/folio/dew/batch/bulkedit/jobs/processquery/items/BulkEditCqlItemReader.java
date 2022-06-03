4b31ef9ddf7ccdb4dd4348feec49f18ba6709492
Illia Daliek <illia_daliek@epam.com>
2022-06-03 14:21:04 +0300

    remove unused imports

--- src/main/java/org/folio/dew/batch/bulkedit/jobs/processquery/items/BulkEditCqlItemReader.java
+++ src/main/java/org/folio/dew/batch/bulkedit/jobs/processquery/items/BulkEditCqlItemReader.java
@@ -3,9 +3,7 @@ package org.folio.dew.batch.bulkedit.jobs.processquery.items;
 import java.util.List;
 import org.folio.dew.batch.CsvItemReader;
 import org.folio.dew.client.InventoryClient;
-import org.folio.dew.client.UserClient;
 import org.folio.dew.domain.dto.Item;
-import org.folio.dew.domain.dto.User;
 
 public class BulkEditCqlItemReader extends CsvItemReader<Item> {
 

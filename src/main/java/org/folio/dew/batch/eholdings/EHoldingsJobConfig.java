bf1ffc817f2413f7761e8a830563bc5a58a56991
psmagin <Pavlo_Smahin@epam.com>
2022-06-03 14:54:35 +0300

    MODEXPW-120 Improve tests

--- src/main/java/org/folio/dew/batch/eholdings/EHoldingsJobConfig.java
+++ src/main/java/org/folio/dew/batch/eholdings/EHoldingsJobConfig.java
@@ -1,14 +1,16 @@
 package org.folio.dew.batch.eholdings;
 
-import java.util.ArrayList;
-import java.util.stream.Collectors;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.apache.commons.lang3.StringUtils;
+import org.folio.dew.batch.JobCompletionNotificationListener;
+import org.folio.dew.client.KbEbscoClient;
 import org.folio.dew.client.NotesClient;
+import org.folio.dew.domain.dto.EHoldingsExportConfig;
+import org.folio.dew.domain.dto.EHoldingsResourceExportFormat;
+import org.folio.dew.domain.dto.ExportType;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
@@ -17,17 +19,10 @@ import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import org.folio.dew.batch.JobCompletionNotificationListener;
-import org.folio.dew.client.KbEbscoClient;
-import org.folio.dew.domain.dto.EHoldingsExportConfig;
-import org.folio.dew.domain.dto.EHoldingsResourceExportFormat;
-import org.folio.dew.domain.dto.ExportType;
 
 @Log4j2
 @Configuration
@@ -59,9 +54,10 @@ public class EHoldingsJobConfig {
 
   @Bean("getEHoldingsStep")
   public Step getEHoldingsStep(@Qualifier("eHoldingsWriter") EholdingsCsvWriter flatFileItemWriter,
-    @Qualifier("eHoldingsNoteProcessor") ItemProcessor<EHoldingsResourceExportFormat, EHoldingsResourceExportFormat> processor,
-    EHoldingsItemReader eHoldingsCsvItemReader,
-    EHoldingsStepListener eHoldingsStepListener) {
+                               @Qualifier("eHoldingsNoteProcessor")
+                               ItemProcessor<EHoldingsResourceExportFormat, EHoldingsResourceExportFormat> processor,
+                               EHoldingsItemReader eHoldingsCsvItemReader,
+                               EHoldingsStepListener eHoldingsStepListener) {
     return stepBuilderFactory
       .get("getEHoldingsStep")
       .<EHoldingsResourceExportFormat, EHoldingsResourceExportFormat>chunk(PROCESSING_RECORD_CHUNK_SIZE)
@@ -83,7 +79,8 @@ public class EHoldingsJobConfig {
   @Bean("eHoldingsNoteProcessor")
   @StepScope
   public NoteEholdingsItemProcessor noteEholdingsItemProcessor(EHoldingsToExportFormatMapper mapper,
-    @Value("#{jobParameters['eHoldingsExportConfig']}") String exportConfigStr) throws JsonProcessingException {
+                                                               @Value("#{jobParameters['eHoldingsExportConfig']}")
+                                                               String exportConfigStr) throws JsonProcessingException {
     var config = objectMapper.readValue(exportConfigStr, EHoldingsExportConfig.class);
     var loadPackageNotes = config.getPackageFields() != null && config.getPackageFields().contains("packageNotes");
     var loadResourceNotes = config.getTitleFields() != null && config.getTitleFields().contains("titleNotes");
--- src/main/java/org/folio/dew/batch/eholdings/EholdingsCsvWriter.java
+++ src/main/java/org/folio/dew/batch/eholdings/EholdingsCsvWriter.java
@@ -16,16 +16,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import org.folio.dew.domain.dto.EHoldingsResourceExportFormat;
-import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.support.AbstractFileItemWriter;
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.core.io.FileSystemResource;
-import org.springframework.util.Assert;
 import org.springframework.util.ClassUtils;
 
 public class EholdingsCsvWriter extends AbstractFileItemWriter<EHoldingsResourceExportFormat> {
 
+  private static final String PACKAGE_NOTES_FIELD = "packageNotes";
+  private static final String TITLE_NOTES_FIELD = "titleNotes";
   private final String[] fieldNames;
 
   public EholdingsCsvWriter(String tempOutputFilePath, String[] fieldNames) {
@@ -34,22 +34,21 @@ public class EholdingsCsvWriter extends AbstractFileItemWriter<EHoldingsResource
       throw new IllegalArgumentException("tempOutputFilePath is blank");
     }
     setResource(new FileSystemResource(tempOutputFilePath));
-//    setAppendAllowed(true);
     this.setExecutionContextName(ClassUtils.getShortName(EholdingsCsvWriter.class));
   }
 
   @Override
-  public void afterPropertiesSet() throws Exception {
+  public void afterPropertiesSet() {
     if (append) {
       shouldDeleteIfExists = false;
     }
   }
 
   private List<String> header(String fieldName, int maxPackageNotesLength, int maxTitleNotesLength) {
-    if (fieldName.equals("packageNotes")) {
-      return getHeadersList(maxPackageNotesLength, "Package Notes");
-    } else if (fieldName.equals("titleNotes")) {
-      return getHeadersList(maxTitleNotesLength, "Title Notes");
+    if (fieldName.equals(PACKAGE_NOTES_FIELD)) {
+      return getHeadersList(maxPackageNotesLength, "Package Note");
+    } else if (fieldName.equals(TITLE_NOTES_FIELD)) {
+      return getHeadersList(maxTitleNotesLength, "Title Note");
     } else {
       return List.of(capitalize(join(splitByCharacterTypeCamelCase(fieldName), SPACE)));
     }
@@ -72,37 +71,44 @@ public class EholdingsCsvWriter extends AbstractFileItemWriter<EHoldingsResource
     for (String fieldName : fieldNames) {
       var value = bw.getPropertyValue(fieldName);
       if (value instanceof String) {
-        var s = (String) value;
-        if (s.contains(",") || s.contains("\n")) {
-          s = "\"" + s.replace("\"", "\"\"").replace(LINE_BREAK, LINE_BREAK_REPLACEMENT) + "\"";
-        }
+        String s = getStringValue((String) value);
         itemValues.add(s);
       } else if (value instanceof List) {
-        var l = (List<String>) value;
-        for (String s : l) {
-          if (s.contains(",") || s.contains("\n")) {
-            s = "\"" + s.replace("\"", "\"\"").replace(LINE_BREAK, LINE_BREAK_REPLACEMENT) + "\"";
-          }
-          itemValues.add(s);
-        }
-        if (fieldName.equals("packageNotes")) {
-          if (l.size() < maxPackageNotesLength) {
-            for (int i = 0; i < maxPackageNotesLength - l.size(); i++) {
-              itemValues.add(EMPTY);
-            }
-          }
-        } else if (fieldName.equals("titleNotes")) {
-          if (l.size() < maxTitleNotesLength) {
-            for (int i = 0; i < maxTitleNotesLength - l.size(); i++) {
-              itemValues.add(EMPTY);
-            }
-          }
-        }
-      } else if (value == null) {
+        @SuppressWarnings("unchecked")
+        var strings = (List<String>) value;
+        getListValue(maxPackageNotesLength, maxTitleNotesLength, itemValues, fieldName, strings);
+      } else {
         itemValues.add(EMPTY);
       }
     }
     return String.join(",", itemValues);
+  }
+
+  private void getListValue(Integer maxPackageNotesLength, Integer maxTitleNotesLength, ArrayList<String> itemValues,
+                            String fieldName, List<String> value) {
+    for (String s : value) {
+      if (s.contains(",") || s.contains("\n")) {
+        s = "\"" + s.replace("\"", "\"\"").replace(LINE_BREAK, LINE_BREAK_REPLACEMENT) + "\"";
+      }
+      itemValues.add(s);
+    }
+    if (fieldName.equals(PACKAGE_NOTES_FIELD) && value.size() < maxPackageNotesLength) {
+      for (int i = 0; i < maxPackageNotesLength - value.size(); i++) {
+        itemValues.add(EMPTY);
+      }
+    } else if (fieldName.equals(TITLE_NOTES_FIELD) && value.size() < maxTitleNotesLength) {
+      for (int i = 0; i < maxTitleNotesLength - value.size(); i++) {
+        itemValues.add(EMPTY);
+      }
+    }
+  }
+
+  private String getStringValue(String value) {
+    var s = value;
+    if (s.contains(",") || s.contains("\n")) {
+      s = "\"" + s.replace("\"", "\"\"").replace(LINE_BREAK, LINE_BREAK_REPLACEMENT) + "\"";
+    }
+    return s;
   }
 
   @Override

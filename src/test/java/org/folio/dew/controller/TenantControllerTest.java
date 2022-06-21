afde0d72652d9078fe5fe92b507eb39a4c2e8fcf
Shans Kaluhin <88006020+Shans-Kaluhin@users.noreply.github.com>
2022-06-21 19:47:21 +0300

    MODEXPW-160 Bad SQL Grammar while disabling module (#247)
    * Implement TenantService
    
    * Disable tenant test
    
    * Implement schema searching for HSQLDB
    
    * Use postgres dialect
    
    * Add constant for sql query

--- src/test/java/org/folio/dew/controller/TenantControllerTest.java
+++ src/test/java/org/folio/dew/controller/TenantControllerTest.java
@@ -3,27 +3,29 @@ package org.folio.dew.controller;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
 import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
-import static org.folio.dew.utils.Constants.MODULE_NAME;
-import static org.folio.dew.utils.Constants.STATUSES_CONFIG_NAME;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import static org.folio.dew.utils.Constants.MODULE_NAME;
+import static org.folio.dew.utils.Constants.STATUSES_CONFIG_NAME;
+
+import java.util.List;
+
 import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+
 import org.folio.dew.BaseBatchTest;
 import org.folio.dew.domain.dto.ModelConfiguration;
 import org.folio.dew.service.BulkEditConfigurationService;
 import org.folio.spring.integration.XOkapiHeaders;
 import org.folio.tenant.domain.dto.TenantAttributes;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
 
 class TenantControllerTest extends BaseBatchTest {
+
   @Test
   @SneakyThrows
-  void canDeleteTenantTest() {
+  void canDisableTenantTest() {
     var headers = defaultHeaders();
     headers.put(XOkapiHeaders.TENANT, List.of("test_tenant"));
 
@@ -31,7 +33,7 @@ class TenantControllerTest extends BaseBatchTest {
       .headers(headers)
       .contentType(APPLICATION_JSON)).andExpect(status().isNoContent());
 
-    mockMvc.perform(delete("/_/tenant/test_tenant")
+    mockMvc.perform(post("/_/tenant").content(asJsonString(new TenantAttributes().purge(true)))
       .headers(headers)
       .contentType(APPLICATION_JSON)).andExpect(status().isNoContent());
   }

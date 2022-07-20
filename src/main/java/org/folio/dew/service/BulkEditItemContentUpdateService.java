378171639b783c8028ca3c4b5b989c5ade525478
siarhei-charniak <siarhei_charniak@epam.com>
2022-07-20 11:40:19 +0300

    MODEXPW-192 - 500 Error occurred Clearing Items locations on the MG bugfest (#265)

--- src/main/java/org/folio/dew/service/BulkEditItemContentUpdateService.java
+++ src/main/java/org/folio/dew/service/BulkEditItemContentUpdateService.java
@@ -76,6 +76,7 @@ public class BulkEditItemContentUpdateService {
       repository.downloadObject(FilenameUtils.getName(jobCommand.getJobParameters().getString(TEMP_OUTPUT_FILE_PATH)) + CSV_EXTENSION, outputFileName);
       var updateResult = new ItemUpdatesResult();
       var records = CsvHelper.readRecordsFromFile(outputFileName, ItemFormat.class, true);
+      log.info("Reading of file {} complete, number of itemFormats: {}", outputFileName, records.size());
       updateResult.setTotal(records.size());
       var contentUpdated = applyContentUpdates(records, contentUpdates, jobCommand);
       updateResult.setItemsForPreview(contentUpdated.getPreview());
@@ -94,6 +95,7 @@ public class BulkEditItemContentUpdateService {
   private void saveResultToFile(List<ItemFormat> itemFormats, JobCommand jobCommand, String outputFileName, String propertyValue) {
     try {
       CsvHelper.saveRecordsToCsv(itemFormats, ItemFormat.class, outputFileName);
+      log.info("Saved {}", outputFileName);
       jobCommand.setJobParameters(new JobParametersBuilder(jobCommand.getJobParameters())
         .addString(propertyValue, outputFileName)
         .toJobParameters());
@@ -107,6 +109,7 @@ public class BulkEditItemContentUpdateService {
   private ContentUpdateRecords applyContentUpdates(List<ItemFormat> itemFormats, ContentUpdateCollection contentUpdates, JobCommand jobCommand) {
     var result = new ContentUpdateRecords();
     for (ItemFormat itemFormat: itemFormats) {
+      log.info("Applying updates to item id={}", itemFormat.getId());
       var updatedItemFormat = itemFormat;
       var errorMessage = new ErrorMessage();
       for (ContentUpdate contentUpdate: contentUpdates.getContentUpdates()) {

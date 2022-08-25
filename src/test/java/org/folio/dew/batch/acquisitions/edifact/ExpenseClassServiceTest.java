651f5ed27f6c6feca1927861acb568e3fb1765f8
Dzmitry_Butramyou <dzmitry_butramyou@epam.com>
2022-08-25 13:27:49 +0400

    [MODEXPW-226] - Test added

--- src/test/java/org/folio/dew/batch/acquisitions/edifact/ExpenseClassServiceTest.java
+++ src/test/java/org/folio/dew/batch/acquisitions/edifact/ExpenseClassServiceTest.java
@@ -16,4 +16,10 @@ class ExpenseClassServiceTest extends BaseBatchTest {
     String expenseClassCode = expenseClassService.getExpenseClassCode("1bcc3247-99bf-4dca-9b0f-7bc51a2998c2");
     assertEquals("Elec", expenseClassCode);
   }
+
+  @Test
+  void shouldReturnEmptyString() {
+    String expenseClassCode = expenseClassService.getExpenseClassCode("2bcc3250-99bf-4dyr-9b0f-9bc51a3098a4");
+    assertEquals("", expenseClassCode);
+  }
 }

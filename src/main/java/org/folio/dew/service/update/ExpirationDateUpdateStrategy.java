4bae23f11abb85ca24c77402e7c3cc2a5ee0cf64
siarhei-charniak <siarhei_charniak@epam.com>
2022-09-05 21:14:50 +0300

    improved user "active" field processing logic

--- src/main/java/org/folio/dew/service/update/ExpirationDateUpdateStrategy.java
+++ src/main/java/org/folio/dew/service/update/ExpirationDateUpdateStrategy.java
@@ -1,13 +1,20 @@
 package org.folio.dew.service.update;
 
+import static org.folio.dew.utils.BulkEditProcessorHelper.dateFromString;
+
 import org.folio.dew.domain.dto.UserContentUpdate;
 import org.folio.dew.domain.dto.UserFormat;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Component
 public class ExpirationDateUpdateStrategy implements UpdateStrategy<UserFormat, UserContentUpdate> {
   @Override
   public UserFormat applyUpdate(UserFormat userFormat, UserContentUpdate update) {
-    return userFormat.withExpirationDate(update.getActions().get(0).getValue().toString());
+    var action = update.getActions().get(0);
+    return userFormat
+      .withExpirationDate(action.getValue().toString())
+      .withActive(Boolean.toString(new Date().before(dateFromString(action.getValue().toString()))));
   }
 }
--- src/test/java/org/folio/dew/service/update/ExpirationDateUpdateStrategyTest.java
+++ src/test/java/org/folio/dew/service/update/ExpirationDateUpdateStrategyTest.java
@@ -1,6 +1,8 @@
 package org.folio.dew.service.update;
 
+import static java.time.ZoneOffset.UTC;
 import static org.folio.dew.utils.BulkEditProcessorHelper.dateToString;
+import static org.folio.dew.utils.Constants.DATE_TIME_PATTERN;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -8,36 +10,44 @@ import org.folio.dew.domain.dto.UserContentUpdate;
 import org.folio.dew.domain.dto.UserContentUpdateAction;
 import org.folio.dew.domain.dto.UserFormat;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.Date;
 
 class ExpirationDateUpdateStrategyTest {
-  private final static String NEW_VALUE = dateToString(new Date());
+  private final static String CURRENT_DATE = dateToString(new Date());
   private final ExpirationDateUpdateStrategy updateStrategy = new ExpirationDateUpdateStrategy();
 
-  @Test
-  void shouldUpdateExpirationDate() {
-    var userFormat = new UserFormat().withExpirationDate("2022-01-01 12:00:00.123Z");
+  @ParameterizedTest
+  @ValueSource(booleans = {true, false})
+  void shouldUpdateExpirationDateAndActiveValue(boolean isActive) {
+    var userFormat = new UserFormat().withExpirationDate(CURRENT_DATE);
+    var newDate = isActive ?
+        dateToString(Date.from(LocalDateTime.now().plusDays(1).atZone(UTC).toInstant())) :
+        dateToString(Date.from(LocalDateTime.now().minusDays(1).atZone(UTC).toInstant()));
     var contentUpdate = new UserContentUpdate()
       .option(UserContentUpdate.OptionEnum.EXPIRATION_DATE)
       .actions(Collections.singletonList(new UserContentUpdateAction()
         .name(UserContentUpdateAction.NameEnum.REPLACE_WITH)
-        .value(NEW_VALUE)));
-
+        .value(newDate)));
     var updatedUserFormat = updateStrategy.applyUpdate(userFormat, contentUpdate);
 
-    assertThat(updatedUserFormat.getExpirationDate(), equalTo(NEW_VALUE));
+    assertThat(updatedUserFormat.getExpirationDate(), equalTo(newDate));
+    assertThat(updatedUserFormat.getActive(), equalTo(Boolean.toString(isActive)));
   }
 
   @Test
   void shouldNotUpdateExpirationDateIfValuesAreEqual() {
-    var userFormat = new UserFormat().withExpirationDate(NEW_VALUE);
+    var userFormat = new UserFormat().withExpirationDate(CURRENT_DATE);
     var contentUpdate = new UserContentUpdate()
       .option(UserContentUpdate.OptionEnum.EXPIRATION_DATE)
       .actions(Collections.singletonList(new UserContentUpdateAction()
         .name(UserContentUpdateAction.NameEnum.REPLACE_WITH)
-        .value(NEW_VALUE)));
+        .value(CURRENT_DATE)));
 
     assertThat(userFormat, equalTo(updateStrategy.applyUpdate(userFormat, contentUpdate)));
   }

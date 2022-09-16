248954687d5a56775153eb13371d65308378e3fb
viachaslau_khandramai <Viachaslau_Khandramai@epam.com>
2022-09-16 11:29:55 +0300

    Merge remote-tracking branch 'origin/MODEXPW-233' into MODEXPW-233
    # Conflicts:
    #	src/main/java/org/folio/dew/batch/bulkedit/jobs/updatejob/BulkEditUpdateHoldingsRecordsJobConfig.java

--- src/main/java/org/folio/dew/batch/marc/MarcWriter.java
+++ src/main/java/org/folio/dew/batch/marc/MarcWriter.java
@@ -3,7 +3,6 @@ package org.folio.dew.batch.marc;
 import lombok.extern.log4j.Log4j2;
 import org.folio.dew.batch.AbstractStorageStreamWriter;
 import org.folio.dew.repository.LocalFilesStorage;
-import org.folio.dew.repository.S3CompatibleResource;
 import org.marc4j.marc.Record;
 
 @Log4j2

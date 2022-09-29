b6a7e5668e3805e46cb0d2d4960334baa5e30eaa
obozhko-folio <83696213+obozhko-folio@users.noreply.github.com>
2022-09-29 23:31:36 +0300

    MODEXPW-250 Added option to change any part of email

--- src/main/java/org/folio/dew/service/update/EmailUpdateStrategy.java
+++ src/main/java/org/folio/dew/service/update/EmailUpdateStrategy.java
@@ -13,38 +13,11 @@ import java.util.Arrays;
 public class EmailUpdateStrategy implements UpdateStrategy<UserFormat, UserContentUpdate> {
   @Override
   public UserFormat applyUpdate(UserFormat userFormat, UserContentUpdate update) {
-    var email = new Email(userFormat.getEmail());
     var findValue = update.getActions().get(0).getValue().toString();
     var replaceWithValue = update.getActions().get(1).getValue().toString();
-    if (email.domainMatch(findValue)) {
-      return userFormat.withEmail(email.asStringWithDomain(findValue, replaceWithValue));
+    if (userFormat.getEmail().contains(findValue)) {
+      return userFormat.withEmail(userFormat.getEmail().replace(findValue, replaceWithValue));
     }
     throw new BulkEditException("Email does not match find criteria");
-  }
-
-  static class Email {
-    private final String localPart;
-    private final String domain;
-    private final String [] domainTokens;
-
-    public Email(String emailString) {
-      var tokens = emailString.split("@");
-      if (tokens.length != 2) {
-        String msg = "Invalid email address: " + emailString;
-        log.error(msg);
-        throw new BulkEditException(msg);
-      }
-      localPart = tokens[0];
-      domain = tokens[1];
-      domainTokens = domain.split("[.]");
-    }
-
-    public boolean domainMatch(String value) {
-      return domain.equals(value) || Arrays.stream(domainTokens).anyMatch(t -> t.equals(value));
-    }
-
-    public String asStringWithDomain(String findValue, String replaceWithValue) {
-      return String.join("@", localPart, domain.replace(findValue, replaceWithValue));
-    }
   }
 }

864dc52a5daa35be042608cc5787fab746eb6b16
Shans Kaluhin <shans_kaluhin@epam.com>
2022-10-04 11:14:25 +0300

    update sql script

--- src/main/resources/db/changelog/changes/create_eholdings_package_table.sql
+++ src/main/resources/db/changelog/changes/create_eholdings_package_table.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS e_holdings_package (
     id VARCHAR(50) NOT NULL,
     job_execution_id BIGINT NOT NULL,
     e_package TEXT,
+    e_provider TEXT,
     agreements TEXT,
     notes TEXT,
     PRIMARY KEY(job_execution_id, id)

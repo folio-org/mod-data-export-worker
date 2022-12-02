2cdd8c445a7900fe86f2baa462f0e728af796b6b
siarhei-charniak <siarhei_charniak@epam.com>
2022-12-02 18:54:27 +0300

    fixed tests

--- src/main/java/org/folio/dew/batch/bulkedit/jobs/processidentifiers/UserFetcher.java
+++ src/main/java/org/folio/dew/batch/bulkedit/jobs/processidentifiers/UserFetcher.java
@@ -9,7 +9,6 @@ import org.folio.dew.client.UserClient;
 import org.folio.dew.domain.dto.ItemIdentifier;
 import org.folio.dew.domain.dto.User;
 import org.folio.dew.error.BulkEditException;
-import org.folio.dew.error.NotFoundException;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.beans.factory.annotation.Value;
@@ -35,11 +34,10 @@ public class UserFetcher implements ItemProcessor<ItemIdentifier, User> {
       throw new BulkEditException("Duplicate entry");
     }
     identifiersToCheckDuplication.add(itemIdentifier);
-    try {
-      return userClient.getUserByQuery(String.format("%s==\"%s\"", resolveIdentifier(identifierType), itemIdentifier.getItemId()), 1).getUsers().get(0);
-    } catch (NotFoundException e) {
-      log.error(NO_MATCH_FOUND_MESSAGE);
+    var userCollection = userClient.getUserByQuery(String.format("%s==\"%s\"", resolveIdentifier(identifierType), itemIdentifier.getItemId()), 1);
+    if (userCollection.getUsers().isEmpty()) {
       throw new BulkEditException(NO_MATCH_FOUND_MESSAGE);
     }
+    return userCollection.getUsers().get(0);
   }
 }

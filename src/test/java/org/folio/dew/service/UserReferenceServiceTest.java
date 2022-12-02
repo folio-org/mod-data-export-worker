f888cda6857201cd9d4f9edde52bec348fef64c6
Aliaksei_Harbuz <Aliaksei_Harbuz@epam.com>
2022-12-02 09:24:35 +0300

    MODEXPW-320 - user reference service test
    (cherry picked from commit 6f541542a58d695c0cacfc3aa50e53970471253a)

---
+++ src/test/java/org/folio/dew/service/UserReferenceServiceTest.java
@@ -0,0 +1,55 @@
+package org.folio.dew.service;
+
+import org.folio.dew.client.AddressTypeClient;
+import org.folio.dew.client.DepartmentClient;
+import org.folio.dew.domain.dto.AddressTypeCollection;
+import org.folio.dew.domain.dto.Department;
+import org.folio.dew.domain.dto.DepartmentCollection;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class UserReferenceServiceTest {
+
+  @Mock
+  private AddressTypeClient addressTypeClient;
+  @Mock
+  private DepartmentClient departmentClient;
+
+  @InjectMocks
+  private UserReferenceService userReferenceService;
+
+  @Test
+  void getAddressTypeByDescTest() {
+    when(addressTypeClient.getAddressTypeByQuery("desc=\"name\"")).thenReturn(new AddressTypeCollection());
+
+    userReferenceService.getAddressTypeByDesc("name");
+
+    verify(addressTypeClient).getAddressTypeByQuery("desc=\"name\"");
+  }
+
+  @Test
+  void getDepartmentByIdTest() {
+    when(departmentClient.getDepartmentById("id")).thenReturn(new Department());
+
+    userReferenceService.getDepartmentById("id");
+
+    verify(departmentClient).getDepartmentById("id");
+  }
+
+  @Test
+  void getDepartmentByNameTest() {
+    when(departmentClient.getDepartmentByQuery("name=\"name\"")).thenReturn(new DepartmentCollection());
+
+    userReferenceService.getDepartmentByName("name");
+
+    verify(departmentClient).getDepartmentByQuery("name=\"name\"");
+  }
+
+}

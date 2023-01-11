17440ee7a9900a7e0bdc1a903aa31bec81f42b49
viachaslau_khandramai <Viachaslau_Khandramai@epam.com>
2023-01-11 18:59:24 +0300

    MODEXPW-347 - Improve json saving process

--- src/main/java/org/folio/dew/batch/CsvAndJsonListWriter.java
+++ src/main/java/org/folio/dew/batch/CsvAndJsonListWriter.java
@@ -3,14 +3,12 @@ package org.folio.dew.batch;
 import org.folio.dew.domain.dto.Formatable;
 import org.folio.dew.repository.S3CompatibleResource;
 import org.folio.dew.repository.S3CompatibleStorage;
-import org.springframework.core.io.WritableResource;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class CsvAndJsonListWriter<O, T extends Formatable<O>, R extends S3CompatibleStorage> extends AbstractStorageStreamWriter<List<T>, R> {
   private final CsvAndJsonWriter<O, T, R> delegate;
-  private WritableResource resource;
 
   public CsvAndJsonListWriter(String tempOutputFilePath, String columnHeaders, String[] extractedFieldNames, FieldProcessor fieldProcessor, R storage) {
     super(tempOutputFilePath, columnHeaders, extractedFieldNames, fieldProcessor, storage);

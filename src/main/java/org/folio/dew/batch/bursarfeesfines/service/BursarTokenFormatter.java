4ab4b14e01757fbca6d0830aafdb24ccd6ab9a84
GoZaddy <faruqyusuff437@gmail.com>
2023-03-13 20:59:20 -0500

    added support for date data token - #34

--- src/main/java/org/folio/dew/batch/bursarfeesfines/service/BursarTokenFormatter.java
+++ src/main/java/org/folio/dew/batch/bursarfeesfines/service/BursarTokenFormatter.java
@@ -1,11 +1,19 @@
 package org.folio.dew.batch.bursarfeesfines.service;
 
+import java.time.DayOfWeek;
 import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.IsoFields;
+import java.time.temporal.WeekFields;
+
 import javax.annotation.CheckForNull;
 import lombok.experimental.UtilityClass;
 import lombok.extern.log4j.Log4j2;
 import org.folio.dew.domain.dto.BursarExportDataToken;
 import org.folio.dew.domain.dto.BursarExportTokenConstant;
+import org.folio.dew.domain.dto.BursarExportTokenDate;
 import org.folio.dew.domain.dto.BursarExportTokenLengthControl;
 import org.folio.dew.domain.dto.bursarfeesfines.AccountWithAncillaryData;
 
@@ -20,6 +28,57 @@ public class BursarTokenFormatter {
     if (token instanceof BursarExportTokenConstant) {
       BursarExportTokenConstant tokenConstant = (BursarExportTokenConstant) token;
       return tokenConstant.getValue();
+    } else if (token instanceof BursarExportTokenDate){
+      BursarExportTokenDate tokenDate = (BursarExportTokenDate) token;
+      String result;
+      
+      ZonedDateTime currentDateTime = Instant.now().atZone(ZoneId.of(tokenDate.getTimezone()));
+
+      switch (tokenDate.getValue()){
+        case YEAR_LONG:
+          result = currentDateTime.format(DateTimeFormatter.ofPattern("yyyy"));
+          break;
+        case YEAR_SHORT:
+          result = currentDateTime.format(DateTimeFormatter.ofPattern("yy"));
+          break;
+        case MONTH:
+          result = String.valueOf(currentDateTime.getMonthValue());
+          break;
+        case DATE:
+          result = String.valueOf(currentDateTime.getDayOfMonth());
+          break;
+        case HOUR:
+          result = String.valueOf(currentDateTime.getHour());
+          break;
+        case MINUTE:
+          result = String.valueOf(currentDateTime.getMinute());
+          break;
+        case SECOND:
+          result = String.valueOf(currentDateTime.getSecond());
+          break;
+        case QUARTER:
+          result = currentDateTime.format(DateTimeFormatter.ofPattern("Q"));
+          break;
+        case WEEK_OF_YEAR:
+          result = String.valueOf(currentDateTime.get(WeekFields.of(DayOfWeek.MONDAY, 7).weekOfYear()));
+          break;
+        case WEEK_OF_YEAR_ISO:
+          result = String.valueOf(currentDateTime.get(IsoFields.WEEK_OF_WEEK_BASED_YEAR));
+          break;
+        case WEEK_YEAR:
+          result = currentDateTime.format(DateTimeFormatter.ofPattern("YYYY"));
+          break;
+        case WEEK_YEAR_ISO:
+          result = String.valueOf(currentDateTime.get(IsoFields.WEEK_BASED_YEAR));
+          break;
+        default:
+          result = String.format("[invalid date type %s]", tokenDate.getValue());
+      }
+
+      return applyLengthControl(
+        result,
+        tokenDate.getLengthControl()
+      );
     } else {
       log.error("Unexpected token: ", token);
       return String.format("[placeholder %s]", token.getType());

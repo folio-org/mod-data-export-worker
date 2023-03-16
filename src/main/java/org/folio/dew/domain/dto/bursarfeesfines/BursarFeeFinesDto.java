84d8c02173c5bdaf05a75d4de152dc3acd79278d
Taras_Spashchenko <Taras_Spashchenko@epam.com>
2023-03-16 13:27:48 +0100

    MODEXPW-388 bugfix

--- src/main/java/org/folio/dew/domain/dto/bursarfeesfines/BursarFeeFinesDto.java
+++ src/main/java/org/folio/dew/domain/dto/bursarfeesfines/BursarFeeFinesDto.java
@@ -2,19 +2,20 @@ package org.folio.dew.domain.dto.bursarfeesfines;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Min;
-import lombok.Data;
-import lombok.NonNull;
-import org.folio.dew.domain.dto.BursarFeeFinesTypeMapping;
-
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.folio.dew.domain.dto.BursarFeeFinesTypeMapping;
 
 @Data
+@NoArgsConstructor
 public class BursarFeeFinesDto {
 
   @JsonProperty("daysOutstanding")

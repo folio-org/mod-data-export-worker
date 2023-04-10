0a85eddc1059a690e27e4a2403f929a228977ca5
nhanaa <nhanpro7d@gmail.com>
2023-04-10 15:37:17 -0500

    Add findTotalAmount() method to AggregatedAccounts class

--- src/main/java/org/folio/dew/domain/dto/bursarfeesfines/AggregatedAccountsByUser.java
+++ src/main/java/org/folio/dew/domain/dto/bursarfeesfines/AggregatedAccountsByUser.java
@@ -1,5 +1,6 @@
 package org.folio.dew.domain.dto.bursarfeesfines;
 
+import java.math.BigDecimal;
 import java.util.List;
 import lombok.Builder;
 import lombok.Data;
@@ -12,4 +13,13 @@ public class AggregatedAccountsByUser {
 
   private User user;
   private List<Account> accounts;
+
+  public BigDecimal findTotalAmount() {
+    BigDecimal totalAmount = new BigDecimal(0);
+    for (Account account : accounts) {
+      totalAmount = totalAmount.add(account.getAmount());
+    }
+
+    return totalAmount;
+  }
 }

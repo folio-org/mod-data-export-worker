06b863ef1b8c820157978ca3ffafc37ad5fad519
nhanaa <nhanpro7d@gmail.com>
2023-04-11 19:48:00 -0500

    Add check for groupByPatron for conditional flow

--- src/main/java/org/folio/dew/batch/bursarfeesfines/PrepareContextTasklet.java
+++ src/main/java/org/folio/dew/batch/bursarfeesfines/PrepareContextTasklet.java
@@ -7,6 +7,7 @@ import lombok.extern.log4j.Log4j2;
 import org.folio.dew.batch.ExecutionContextUtils;
 import org.folio.dew.batch.bursarfeesfines.service.BursarExportUtils;
 import org.folio.dew.domain.dto.BursarExportJob;
+import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.StepExecutionListener;
@@ -23,6 +24,7 @@ import org.springframework.stereotype.Component;
 public class PrepareContextTasklet implements Tasklet, StepExecutionListener {
 
   private final ObjectMapper objectMapper;
+  private boolean isAggregateJob;
 
   @Override
   public void beforeStep(StepExecution stepExecution) {
@@ -45,6 +47,8 @@ public class PrepareContextTasklet implements Tasklet, StepExecutionListener {
         .getJobExecution()
         .getExecutionContext()
         .put("jobConfig", jobConfig);
+
+      isAggregateJob = jobConfig.getGroupByPatron();
     } catch (JsonProcessingException e) {
       log.error("Could not parse job config... ", e);
     }
@@ -55,6 +59,18 @@ public class PrepareContextTasklet implements Tasklet, StepExecutionListener {
     StepContribution contribution,
     ChunkContext chunkContext
   ) {
+    if (!isAggregateJob) {
+      chunkContext
+        .getStepContext()
+        .getStepExecution()
+        .setExitStatus(new ExitStatus("NOT AGGREGATE"));
+    } else {
+      log.info("Is aggregate job");
+      chunkContext
+        .getStepContext()
+        .getStepExecution()
+        .setExitStatus(new ExitStatus("IS AGGREGATE"));
+    }
     return RepeatStatus.FINISHED;
   }
 }

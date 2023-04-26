bbc5b06565a0ffc4e08d8034b2e5336740ee5b23
GoZaddy <faruqyusuff437@gmail.com>
2023-04-26 17:08:10 -0500

    closes #86

--- src/main/java/org/folio/dew/batch/bursarfeesfines/service/BursarTokenFormatter.java
+++ src/main/java/org/folio/dew/batch/bursarfeesfines/service/BursarTokenFormatter.java
@@ -177,7 +177,16 @@ public class BursarTokenFormatter {
       BursarExportTokenUserData.ValueEnum.EXTERNAL_SYSTEM_ID
     ) {
       result = user.getExternalSystemId();
-    } else {
+    } else if (
+      tokenUserData.getValue() == BursarExportTokenUserData.ValueEnum.CORNELL_EXTERNAL_SYSTEM_ID
+    ){
+      result = user.getExternalSystemId();
+
+      // get last 7 digits which matches employee's Cornell ID(EmpIID)
+      if (result.length() > 7){
+        result = result.substring(result.length() - 7);
+      }
+    }else {
       result =
         String.format(
           "[unexpected user data token: %s]",

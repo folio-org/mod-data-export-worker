c04ccbef28db8fe85d7bbac142ff2f7600c62455
Dmytro_Bykov <dmytro_bykov@epam.com>
2024-03-04 12:59:25 +0500

    MODEXPW-462: spring upgrade

--- src/main/java/org/folio/dew/config/properties/AuthorityControlJobProperties.java
+++ src/main/java/org/folio/dew/config/properties/AuthorityControlJobProperties.java
@@ -1,12 +1,11 @@
 package org.folio.dew.config.properties;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
-
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 
 /**
  * Application properties for authority control batch job configuration

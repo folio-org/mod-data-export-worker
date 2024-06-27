1498be130050ac476a533a8e32a31ff732af1b19
Viachaslau Khandramai <Viachaslau_Khandramai@epam.com>
2024-06-27 13:45:38 +0200

    MODEXPW-477 - Items and holdings tenant populating (folio-export-common schemas bump up)

--- src/main/java/org/folio/dew/service/ConsortiaService.java
+++ src/main/java/org/folio/dew/service/ConsortiaService.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.lang3.StringUtils;
 import org.folio.dew.client.ConsortiaClient;
-import org.folio.spring.FolioExecutionContext;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +12,6 @@ import org.springframework.stereotype.Service;
 @Log4j2
 public class ConsortiaService {
 
-  private final FolioExecutionContext context;
-
   private final ConsortiaClient consortiaClient;
 
   @Cacheable(value = "centralTenantCache")
@@ -22,14 +19,8 @@ public class ConsortiaService {
     var userTenantCollection = consortiaClient.getUserTenantCollection();
     var userTenants = userTenantCollection.getUserTenants();
     if (!userTenants.isEmpty()) {
-      log.info("userTenants: {}", userTenants);
-      var centralTenantId = userTenants.get(0).getCentralTenantId();
-      if (centralTenantId.equals(context.getTenantId())) {
-        log.error("Current tenant is central");
-      }
-      return centralTenantId;
+      return userTenants.get(0).getCentralTenantId();
     }
-    log.info("No central tenant found");
     return StringUtils.EMPTY;
   }
 }

566d8a6a03984405ad68d8b65154a74eec8e0bf9
Azizbek Khushvakov <azizbek_khushvakov@epam.com>
2024-08-09 19:25:15 +0500

    improved test

--- src/test/java/org/folio/dew/batch/acquisitions/edifact/ConfigurationServiceTest.java
+++ src/test/java/org/folio/dew/batch/acquisitions/edifact/ConfigurationServiceTest.java
@@ -3,10 +3,13 @@ package org.folio.dew.batch.acquisitions.edifact;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.UUID;
+import java.util.stream.Stream;
 
 import org.folio.dew.BaseBatchTest;
 import org.folio.dew.batch.acquisitions.edifact.services.ConfigurationService;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
 class ConfigurationServiceTest extends BaseBatchTest {
@@ -14,36 +17,20 @@ class ConfigurationServiceTest extends BaseBatchTest {
   @Autowired
   private ConfigurationService configurationService;
 
-  @Test
-  void getAddress() {
-    String addressConfigId = "1947e709-8d60-42e2-8dde-7566ae446d24"; // don't change, it is hardcoded in configurations.json
-
-    String address = configurationService.getAddressConfig(UUID.fromString(addressConfigId));
-    assertEquals("Address 123", address);
+  static Stream<Arguments> provideAddressConfigData() {
+    return Stream.of(
+      Arguments.of("1947e709-8d60-42e2-8dde-7566ae446d24", "Address 123"), // existing config
+      Arguments.of(null, ""), // null config ID
+      Arguments.of("116a38c2-cac3-4f08-816b-afebfebe453d", ""), // non-existing config
+      Arguments.of("8ea92aa2-7b11-4f0e-9ed2-ab8fe281f37f", "") // config without address value
+    );
   }
 
-  @Test
-  void getAddressConfigByNullId() {
-    String address = configurationService.getAddressConfig(null);
-
-    assertEquals("", address);
-  }
-
-  @Test
-  void getEmptyAddressConfig() {
-    String addressConfigId = "116a38c2-cac3-4f08-816b-afebfebe453d"; // non-existing config
-
-    String address = configurationService.getAddressConfig(UUID.fromString(addressConfigId));
-
-    assertEquals("", address);
-  }
-
-  @Test
-  void getAddressConfigWithoutAddressValue() {
-    String addressConfigId = "8ea92aa2-7b11-4f0e-9ed2-ab8fe281f37f"; // non-existing config
-
-    String address = configurationService.getAddressConfig(UUID.fromString(addressConfigId));
-
-    assertEquals("", address);
+  @ParameterizedTest
+  @MethodSource("provideAddressConfigData")
+  void testGetAddressConfig(String addressConfigId, String expectedAddress) {
+    UUID configId = addressConfigId != null ? UUID.fromString(addressConfigId) : null;
+    String address = configurationService.getAddressConfig(configId);
+    assertEquals(expectedAddress, address);
   }
 }

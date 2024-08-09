873417e9f1c58bfa5bc7bda2c68e97129da722ba
Azizbek Khushvakov <azizbek_khushvakov@epam.com>
2024-08-09 17:46:18 +0500

    Revert "minor improvments"
    This reverts commit 0dad75278e59ebd7e16205b0168886b7baa42845.

--- src/test/java/org/folio/dew/batch/acquisitions/edifact/ConfigurationServiceTest.java
+++ src/test/java/org/folio/dew/batch/acquisitions/edifact/ConfigurationServiceTest.java
@@ -9,13 +9,11 @@ import org.folio.dew.batch.acquisitions.edifact.services.ConfigurationService;
 import org.folio.dew.client.ConfigurationClient;
 import org.folio.dew.domain.dto.ModelConfiguration;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-@ExtendWith(MockitoExtension.class)
+
 class ConfigurationServiceTest extends BaseBatchTest {
 
   @Autowired

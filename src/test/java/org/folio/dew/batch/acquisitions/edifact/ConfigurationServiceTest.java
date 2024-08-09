07a9487a79c03c5aa08ec9fbed32603536da9eb2
Azizbek Khushvakov <azizbek_khushvakov@epam.com>
2024-08-09 18:16:47 +0500

    Fixed test context issue

--- src/test/java/org/folio/dew/batch/acquisitions/edifact/ConfigurationServiceTest.java
+++ src/test/java/org/folio/dew/batch/acquisitions/edifact/ConfigurationServiceTest.java
@@ -6,40 +6,19 @@ import java.util.UUID;
 
 import org.folio.dew.BaseBatchTest;
 import org.folio.dew.batch.acquisitions.edifact.services.ConfigurationService;
-import org.folio.dew.client.ConfigurationClient;
-import org.folio.dew.domain.dto.ModelConfiguration;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
-@ExtendWith(MockitoExtension.class)
 class ConfigurationServiceTest extends BaseBatchTest {
 
   @Autowired
   private ConfigurationService configurationService;
-  @MockBean
-  private ConfigurationClient configurationClient;
 
   @Test
   void getAddress() {
-    String addressConfigId = UUID.randomUUID().toString();
-    var addressConfiguration = new ModelConfiguration()
-      .id(addressConfigId)
-      .configName("tenant.addresses")
-      .value("{\"name\":\"Name 1\",\"address\":\"Address 123\"}");
-    Mockito.when(configurationClient.getConfigById(addressConfigId)).thenReturn(addressConfiguration);
+    String addressConfigId = "1947e709-8d60-42e2-8dde-7566ae446d24"; // don't change it is hard coded
 
     String addressFirst = configurationService.getAddressConfig(UUID.fromString(addressConfigId));
     assertEquals("Address 123", addressFirst);
-
-    // fetching from cache
-    String addressSecond = configurationService.getAddressConfig(UUID.fromString(addressConfigId));
-    assertEquals("Address 123", addressSecond);
-
-    // configurationClient is invoked only one time
-    Mockito.verify(configurationClient, Mockito.times(1)).getConfigById(addressConfigId);
   }
 }

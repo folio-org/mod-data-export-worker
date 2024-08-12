fba02e882a5da36e00eeab3c5d1371ff77dbce4d
Azizbek Khushvakov <113523904+azizbekxm@users.noreply.github.com>
2024-08-12 17:55:33 +0500

    [MODORDERS-1152] - Fix additional fields in EDIFACT order syntax (#556)
    * [MODORDERS-1152] - Fix additional fields in EDIFACT order syntax
    
    * [MODORDERS-1152] - Fix additional fields in EDIFACT order syntax
    
    * [MODORDERS-1152] - Fix additional fields in EDIFACT order syntax
    
    * Implemented Cacheable, updated unit price calculation, fixed unit test
    
    * fixed test
    
    * minor improvements
    
    * minor improvments
    
    * Revert "minor improvments"
    
    This reverts commit 0dad75278e59ebd7e16205b0168886b7baa42845.
    
    * Revert "minor improvements"
    
    This reverts commit 868bb9a0f1d59ddb09df8a49ad327a0d37f40a81.
    
    * Revert "fixed test"
    
    This reverts commit 143cfb23c044b3647a01d455e12159e2249bbeca.
    
    * Revert "Implemented Cacheable, updated unit price calculation, fixed unit test"
    
    This reverts commit 5f4429b2993f872d1af84af40274c36780374b45.
    
    * Reapply "Implemented Cacheable, updated unit price calculation, fixed unit test"
    
    This reverts commit 23707536c40544bb47658dd6ebbada451253d9cb.
    
    * Reapply "fixed test"
    
    This reverts commit fc0291a6022e5e30578945c989d50477943fd59c.
    
    * Reapply "minor improvements"
    
    This reverts commit 876c7b0aae3562319fa451c3e47c31b1f7d00fcf.
    
    * Reapply "minor improvments"
    
    This reverts commit 873417e9f1c58bfa5bc7bda2c68e97129da722ba.
    
    * Fixed test context issue
    
    * Increased code coverage
    
    * Increased code coverage
    
    * improved test
    
    * Changed to Object Mapper and simplified condition
    
    * Changed to Object Mapper and simplified condition
    
    * Fixed possible NP
    
    * Minor improvements
    
    * small fix
    
    * added comment
    
    * added comment
    
    * removed unnessary if statement

--- src/test/java/org/folio/dew/batch/acquisitions/edifact/ConfigurationServiceTest.java
+++ src/test/java/org/folio/dew/batch/acquisitions/edifact/ConfigurationServiceTest.java
@@ -2,19 +2,35 @@ package org.folio.dew.batch.acquisitions.edifact;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.UUID;
+import java.util.stream.Stream;
+
 import org.folio.dew.BaseBatchTest;
 import org.folio.dew.batch.acquisitions.edifact.services.ConfigurationService;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
-
 class ConfigurationServiceTest extends BaseBatchTest {
+
   @Autowired
   private ConfigurationService configurationService;
 
-  @Test
-  void getCurrency() {
-    String currency = configurationService.getSystemCurrency();
-    assertEquals("USD", currency);
+  static Stream<Arguments> provideAddressConfigData() {
+    return Stream.of(
+      Arguments.of("1947e709-8d60-42e2-8dde-7566ae446d24", "Address 123"), // existing config
+      Arguments.of(null, ""), // null config ID
+      Arguments.of("116a38c2-cac3-4f08-816b-afebfebe453d", ""), // non-existing config
+      Arguments.of("8ea92aa2-7b11-4f0e-9ed2-ab8fe281f37f", "") // config without address value
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideAddressConfigData")
+  void testGetAddressConfig(String addressConfigId, String expectedAddress) {
+    UUID configId = addressConfigId != null ? UUID.fromString(addressConfigId) : null;
+    String address = configurationService.getAddressConfig(configId);
+    assertEquals(expectedAddress, address);
   }
 }

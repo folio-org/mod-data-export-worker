a5a5d9f5a62663c9be95db34468429388094a9fa
Aliaksei_Harbuz <Aliaksei_Harbuz@epam.com>
2024-09-11 15:33:29 +0300

    MODEXPW-488 - update permission set

--- src/main/java/org/folio/dew/batch/bulkedit/jobs/permissions/check/RequiredPermissionResolver.java
+++ src/main/java/org/folio/dew/batch/bulkedit/jobs/permissions/check/RequiredPermissionResolver.java
@@ -10,10 +10,12 @@ public class RequiredPermissionResolver {
 
   public Set<String> getReadPermission(EntityType entityType) {
     return switch (entityType) {
-      case USER -> Set.of("ui-users.view");
-      case ITEM -> Set.of("ui-inventory.item.edit");
-      case HOLDINGS_RECORD -> Set.of("ui-inventory.holdings.edit");
-      case INSTANCE -> Set.of("ui-inventory.instance.view");
+      case USER -> Set.of("ui-users.view", "ui-users.edit");
+      case ITEM -> Set.of("ui-inventory.item.edit", "ui-inventory.item.create", "ui-inventory.item.delete", "ui-inventory.instance.view");
+      case HOLDINGS_RECORD -> Set.of("ui-inventory.holdings.edit", "ui-inventory.holdings.create", "ui-inventory.holdings.delete",
+        "ui-inventory.instance.view");
+      case INSTANCE -> Set.of("ui-inventory.instance.create", "ui-inventory.instance.view", "ui-inventory.instance.edit",
+        "ui-quick-marc.quick-marc-editor.view", "ui-quick-marc.quick-marc-editor.all");
     };
   }
 }
--- src/test/java/org/folio/dew/batch/bulkedit/jobs/permissions/check/RequiredPermissionResolverTest.java
+++ src/test/java/org/folio/dew/batch/bulkedit/jobs/permissions/check/RequiredPermissionResolverTest.java
@@ -12,9 +12,9 @@ class RequiredPermissionResolverTest {
   @Test
   void testGetReadPermission() {
     var requiredPermissionResolver = new RequiredPermissionResolver();
-    assertEquals(Set.of("ui-users.view"),  requiredPermissionResolver.getReadPermission(EntityType.USER));
-    assertEquals(Set.of("ui-inventory.item.edit"),  requiredPermissionResolver.getReadPermission(EntityType.ITEM));
-    assertEquals(Set.of("ui-inventory.holdings.edit"),  requiredPermissionResolver.getReadPermission(EntityType.HOLDINGS_RECORD));
-    assertEquals(Set.of("ui-inventory.instance.view"),  requiredPermissionResolver.getReadPermission(EntityType.INSTANCE));
+    assertEquals(Set.of("ui-users.view", "ui-users.edit"),  requiredPermissionResolver.getReadPermission(EntityType.USER));
+    assertEquals(Set.of("ui-inventory.item.edit", "ui-inventory.instance.view", "ui-inventory.item.create", "ui-inventory.item.delete"),  requiredPermissionResolver.getReadPermission(EntityType.ITEM));
+    assertEquals(Set.of("ui-inventory.instance.view", "ui-inventory.holdings.edit", "ui-inventory.holdings.delete", "ui-inventory.holdings.create"),  requiredPermissionResolver.getReadPermission(EntityType.HOLDINGS_RECORD));
+    assertEquals(Set.of("ui-inventory.instance.view", "ui-inventory.instance.create", "ui-inventory.instance.edit", "ui-quick-marc.quick-marc-editor.view", "ui-quick-marc.quick-marc-editor.all"),  requiredPermissionResolver.getReadPermission(EntityType.INSTANCE));
   }
 }

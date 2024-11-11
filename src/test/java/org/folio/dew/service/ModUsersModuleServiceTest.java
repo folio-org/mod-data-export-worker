401bb05f2c2dcd729e0b4e4de81af34ebe9f989a
Aliaksei_Harbuz <Aliaksei_Harbuz@epam.com>
2024-11-11 12:19:26 +0300

    MODEXPW-526 - update tests

--- src/test/java/org/folio/dew/service/ModUsersModuleServiceTest.java
+++ src/test/java/org/folio/dew/service/ModUsersModuleServiceTest.java
@@ -3,7 +3,12 @@ package org.folio.dew.service;
 import static org.folio.dew.utils.Constants.EUREKA_PLATFORM;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.folio.dew.client.OkapiClient;
 import org.folio.dew.error.NotFoundException;
 import org.folio.spring.FolioExecutionContext;
@@ -12,6 +17,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.net.URI;
 
 @ExtendWith(MockitoExtension.class)
 public class ModUsersModuleServiceTest {
@@ -25,11 +32,24 @@ public class ModUsersModuleServiceTest {
   private ModUsersModuleService modUsersModuleService;
 
   @Test
+  void getModUsersModuleIdForOkapiTest() {
+    var tenantId = "diku";
+    JsonNode arrayNode = new com.fasterxml.jackson.databind.node.ArrayNode(new
+      com.fasterxml.jackson.databind.node.JsonNodeFactory(true));
+
+    when(folioExecutionContext.getTenantId()).thenReturn(tenantId);
+    when(okapiClient.getModuleIds(isA(URI.class), eq(tenantId), eq("mod-users"))).thenReturn(arrayNode);
+
+    assertThrows(NotFoundException.class, ()-> modUsersModuleService.getModUsersModuleId());
+    verify(okapiClient).getModuleIds(isA(URI.class), eq(tenantId), eq("mod-users"));
+  }
+
+  @Test
   void getModUsersModuleIdForEurekaTest() {
     var moduleId = "usersModuleId";
     modUsersModuleService.setPlatform(EUREKA_PLATFORM);
 
-    assertThrows(NotFoundException.class, () -> modUsersModuleService.getModUsersModuleId());
+    assertThrows(NotFoundException.class, ()-> modUsersModuleService.getModUsersModuleId());
 
     modUsersModuleService.setModUsersId(moduleId);
     var actual = modUsersModuleService.getModUsersModuleId();

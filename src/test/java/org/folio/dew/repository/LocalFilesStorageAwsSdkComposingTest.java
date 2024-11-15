b38c2d96898bd569a64ef4fcfb018a2852548e7e
julianladisch <julianladisch@users.noreply.github.com>
2024-11-15 11:25:19 +0100

    MODEXPW-500: forcePathStyle fixing UnknownHostException local-files-test.localhost (#561)

--- src/test/java/org/folio/dew/repository/LocalFilesStorageAwsSdkComposingTest.java
+++ src/test/java/org/folio/dew/repository/LocalFilesStorageAwsSdkComposingTest.java
@@ -26,7 +26,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Log4j2
 @SpringBootTest(classes = { LocalFilesStorageProperties.class, LocalFilesStorage.class }, properties = {
-    "application.minio-local.compose-with-aws-sdk = true" })
+    "application.minio-local.compose-with-aws-sdk = true", "application.minio-local.force-path-style = true" })
 @EnableConfigurationProperties
 class LocalFilesStorageAwsSdkComposingTest {
 

c89da5be140270c4a40a6d5490990fa8abfa9ec2
Viachaslau_Khandramai <Viachaslau_Khandramai@epam.com>
2024-12-07 15:31:52 +0300

    MODEXPW-545 - Improve caching

--- src/main/java/org/folio/dew/config/CacheConfig.java
+++ src/main/java/org/folio/dew/config/CacheConfig.java
@@ -1,12 +1,15 @@
 package org.folio.dew.config;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
+import org.folio.dew.domain.dto.ErrorServiceArgs;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
@@ -22,5 +25,12 @@ public class CacheConfig {
     CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
     caffeineCacheManager.setCaffeine(caffeine);
     return caffeineCacheManager;
+  }
+
+  @Bean("defaultKeyGenerator")
+  public KeyGenerator defaultKeyGenerator() {
+    return (target, method, params) -> Arrays.stream(params)
+      .filter(param -> !param.getClass().equals(ErrorServiceArgs.class))
+      .toList();
   }
 }

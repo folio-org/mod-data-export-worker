85950a1915990918e93da04af8253606cfc0c999
Saba-Zedginidze-EPAM <148070844+Saba-Zedginidze-EPAM@users.noreply.github.com>
2024-12-09 20:45:18 +0400

    [MODEXPW-528] Generate EDI file for claims (#601)
    * [MODEXPW-528] Generate EDI file for claims

--- src/main/java/org/folio/dew/error/NotFoundException.java
+++ src/main/java/org/folio/dew/error/NotFoundException.java
@@ -1,7 +1,15 @@
 package org.folio.dew.error;
 
 public class NotFoundException extends RuntimeException {
+
+  private static final String EXCEPTION_MESSAGE = "Entities not found: %s";
+
+  public NotFoundException(Class<?> entityClass) {
+    super(EXCEPTION_MESSAGE.formatted(entityClass.getSimpleName()), null, false, false);
+  }
+
   public NotFoundException(String message) {
     super(message);
   }
+
 }

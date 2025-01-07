8a94c05962a345a84c739603384bf3ca475449d2
obozhko-folio <Oleksandr_Bozhko@epam.com>
2025-01-07 21:32:44 +0200

    MODEXPW-544 Removed possible BOM

--- src/main/java/org/folio/dew/domain/dto/ItemIdentifier.java
+++ src/main/java/org/folio/dew/domain/dto/ItemIdentifier.java
@@ -4,9 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import static org.folio.dew.utils.Constants.UTF8_BOM;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ItemIdentifier {
   private String itemId;
+
+  public void setItemId(String itemId) {
+    if (itemId.startsWith(UTF8_BOM)) {
+      itemId = itemId.substring(1);
+    }
+    this.itemId = itemId;
+  }
 }

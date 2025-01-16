3ef0246d6f7e9d3951175332abf58309beffb34e
obozhko-folio <83696213+obozhko-folio@users.noreply.github.com>
2025-01-16 12:51:56 +0200

    MODEXPW-551 Added check for year

--- src/main/java/org/folio/dew/batch/bulkedit/jobs/processidentifiers/UserFetcher.java
+++ src/main/java/org/folio/dew/batch/bulkedit/jobs/processidentifiers/UserFetcher.java
@@ -1,7 +1,10 @@
 package org.folio.dew.batch.bulkedit.jobs.processidentifiers;
 
 import static java.lang.String.format;
+import static java.util.Objects.nonNull;
+import static org.folio.dew.utils.BulkEditProcessorHelper.dateToString;
 import static org.folio.dew.utils.BulkEditProcessorHelper.resolveIdentifier;
+import static org.folio.dew.utils.Constants.MIN_YEAR_FOR_BIRTH_DATE;
 import static org.folio.dew.utils.Constants.MULTIPLE_MATCHES_MESSAGE;
 import static org.folio.dew.utils.Constants.NO_MATCH_FOUND_MESSAGE;
 import static org.folio.dew.utils.Constants.NO_USER_VIEW_PERMISSIONS;
@@ -22,6 +25,10 @@ import org.springframework.batch.item.ItemProcessor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -62,9 +69,21 @@ public class UserFetcher implements ItemProcessor<ItemIdentifier, User> {
       } else if (userCollection.getTotalRecords() > limit) {
         throw new BulkEditException(MULTIPLE_MATCHES_MESSAGE);
       }
-      return userCollection.getUsers().get(0);
+      var user = userCollection.getUsers().get(0);
+      var birthDate = user.getPersonal().getDateOfBirth();
+      validateBirthDate(birthDate);
+      return user;
     } catch (DecodeException e) {
       throw new BulkEditException(ExceptionHelper.fetchMessage(e));
+    }
+  }
+
+  private void validateBirthDate(Date birthDate) {
+    if (nonNull(birthDate)) {
+      var year = LocalDateTime.ofInstant(Instant.ofEpochMilli(birthDate.getTime()), ZoneOffset.UTC).getYear();
+      if (year < MIN_YEAR_FOR_BIRTH_DATE) {
+        throw new BulkEditException(String.format("Failed to parse Date from value \"%s\" in users.personal.dateOfBirth", dateToString(birthDate)));
+      }
     }
   }
 }

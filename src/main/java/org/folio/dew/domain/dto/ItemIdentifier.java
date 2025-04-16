755921d9c6f9f06d8ec046be6c56196cf5d7c8cd
obozhko-folio <Oleksandr_Bozhko@epam.com>
2025-04-16 20:09:50 +0300

    duplicate-test

--- src/main/java/org/folio/dew/domain/dto/ItemIdentifier.java
+++ src/main/java/org/folio/dew/domain/dto/ItemIdentifier.java
@@ -1,15 +1,17 @@
 package org.folio.dew.domain.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.folio.dew.utils.CsvHelper;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class ItemIdentifier {
   private String itemId;
+
+  public ItemIdentifier(String itemId) {
+    setItemId(itemId);
+  }
 
   public void setItemId(String itemId) {
     this.itemId = CsvHelper.clearBomSymbol(itemId);

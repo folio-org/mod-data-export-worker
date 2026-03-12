8cdaf4398f89dffed20a14507261776515b9dfb8
Markus Weigelt <markus.weigelt@slub-dresden.de>
2026-03-12 10:37:53 +0100

    Improve email implementation

--- src/main/java/org/folio/dew/client/EmailClient.java
+++ src/main/java/org/folio/dew/client/EmailClient.java
@@ -2,13 +2,12 @@ package org.folio.dew.client;
 
 
 import org.folio.dew.domain.dto.email.EmailEntity;
-import org.folio.spring.config.FeignClientConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "email", configuration = FeignClientConfiguration.class)
+@FeignClient(name = "email", url = "${folio.email-url:http://localhost:8000}")
 public interface EmailClient {
 
   @PostMapping(value = "/email", consumes = MediaType.APPLICATION_JSON_VALUE)

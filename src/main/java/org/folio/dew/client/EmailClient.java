c64c9b5c8c14c34d8ca9f1d120542b5ea6c7fa7e
Markus Weigelt <markus.weigelt@slub-dresden.de>
2026-03-12 19:49:51 +0100

    add configuration

--- src/main/java/org/folio/dew/client/EmailClient.java
+++ src/main/java/org/folio/dew/client/EmailClient.java
@@ -2,12 +2,13 @@ package org.folio.dew.client;
 
 
 import org.folio.dew.domain.dto.email.EmailEntity;
+import org.folio.spring.config.FeignClientConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "email")
+@FeignClient(name = "email", configuration = FeignClientConfiguration.class)
 public interface EmailClient {
 
   @PostMapping(value = "/email", consumes = MediaType.APPLICATION_JSON_VALUE)
